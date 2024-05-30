84750cae7f2dfa0dae775b0a8bd840c6dbefd0fc
e-kondra <e-kondra@mail.ru>
2024-05-30 10:24:19 +0300

    commit1 Task1

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, " + (args.length > 0 && args[0].length() > 0? args[0] : "Ekaterina") + "!");
+    }
+
+}
