ac42c9ebf1d5c19deb229db35a71365cf61d5479
e-kondra <e-kondra@mail.ru>
2024-06-06 16:54:33 +0300

    Task3: Create and fill storage

--- src/task3/TicketService.java
+++ src/task3/TicketService.java
@@ -1,18 +1,34 @@
 package task3;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class TicketService {
 
-    public static void main(String[] args) {
-        Ticket ticketEmpty = new Ticket();
-        Ticket ticketFull = new Ticket(1L, "Compensa", 305,
-                false, Sector.A, 2.300f, new BigDecimal(23.56));
-        Ticket ticketLimited = new Ticket("Compensa", 342);
+    private final ArrayList<Ticket> ticketStorage;
 
-        System.out.println(ticketEmpty);
-        System.out.println(ticketFull);
-        System.out.println(ticketLimited);
+    public TicketService() {
+        this.ticketStorage = this.fillInStorage();
+    }
+
+    private ArrayList<Ticket> fillInStorage() {
+        ArrayList tickets = new ArrayList<>();
+        for(int i = 1; i<11 ;i++){
+            Ticket ticket = new Ticket(Long.valueOf(i), "Compensa",
+                    342, false,
+                    Sector.values()[new Random().nextInt(Sector.values().length)],
+                    2.300f, new BigDecimal(23.56));
+            tickets.add(ticket);
+        }
+        return tickets;
+    }
+
+    public static void main(String[] args) {
+        TicketService ticketService = new TicketService();
+        ticketService.ticketStorage.stream().forEach(System.out::println);
 
     }
+
 }
